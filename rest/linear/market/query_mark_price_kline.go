050043f7bfe7b2dc948b5f24f3b002a609d91630
Sid <csidharthank@gmail.com>
2022-08-10 03:23:23 +0200

    fmt changes

--- rest/linear/market/query_mark_price_kline.go
+++ rest/linear/market/query_mark_price_kline.go
@@ -2,9 +2,10 @@ package market
 
 import (
 	"context"
+	"net/http"
+
 	"github.com/cksidharthan/go-bybit/bybit"
 	"github.com/cksidharthan/go-bybit/rest/domain/linear"
-	"net/http"
 )
 
 func (c *Client) QueryMarkPriceKline(ctx context.Context, params *linear.QueryMarkPriceKlineParams) (res *linear.QueryMarkPriceKlineResponse, err error) {
