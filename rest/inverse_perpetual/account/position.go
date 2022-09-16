e1943f44e491b7d92560663bfa92ca07f2464b3f
Sidharthan Chandrasekaran Kamaraj <csidharthank@gmail.com>
2022-09-16 23:05:21 +0200

    add api implementation for inverse perpetual account

--- rest/inverse_perpetual/account/position.go
+++ rest/inverse_perpetual/account/position.go
@@ -82,3 +82,78 @@ func (c *InversePerpetualAccountClient) SetLeverage(ctx context.Context, params 
 	}
 	return
 }
+
+/*
+GetUserTradeRecords - get user trade records. [ /v2/private/execution/list  - GET ]
+
+Get user's trade records.
+
+docs - https://bybit-exchange.github.io/docs/futuresV2/inverse/#t-usertrades
+*/
+func (c *InversePerpetualAccountClient) GetUserTradeRecords(ctx context.Context, params *inverseperp.GetUserTradeRecordsParams) (response *inverseperp.GetUserTradeRecordsResponse, err error) {
+	err = c.Transporter.SignedRequest(ctx, http.MethodGet, bybit.PrivateInversePerpetualGetUserTradeRecordsPath, params, &response)
+	if err != nil {
+		return
+	}
+	return
+}
+
+/*
+ClosedProfitAndLoss - closed profit and loss. [ /v2/private/position/closed-pnl/list  - GET ]
+
+Get user's closed profit and loss records.
+
+docs - https://bybit-exchange.github.io/docs/futuresV2/inverse/#t-closedprofitandloss
+*/
+func (c *InversePerpetualAccountClient) ClosedProfitAndLoss(ctx context.Context, params *inverseperp.ClosedProfitAndLossParams) (response *inverseperp.ClosedProfitAndLossResponse, err error) {
+	err = c.Transporter.SignedRequest(ctx, http.MethodGet, bybit.PrivateInversePerpetualClosedProfitLossPath, params, &response)
+	if err != nil {
+		return
+	}
+	return
+}
+
+/*
+PositionTpSlSwitch - position tp sl switch. [ /v2/private/position/tpsl  - POST ]
+
+Switch user's position's tp/sl.
+
+docs - https://bybit-exchange.github.io/docs/futuresV2/inverse/#t-tpslswitch
+*/
+func (c *InversePerpetualAccountClient) PositionTpSlSwitch(ctx context.Context, params *inverseperp.PositionTpSlSwitchParams) (response *inverseperp.PositionTpSlSwitchResponse, err error) {
+	err = c.Transporter.SignedRequest(ctx, http.MethodPost, bybit.PrivateInversePerpetualPositionTpSlSwitchPath, params, &response)
+	if err != nil {
+		return
+	}
+	return
+}
+
+/*
+MarginSwitch - margin switch. [ /v2/private/position/margin-switch  - POST ]
+
+Switch user's position's margin mode.
+
+docs - https://bybit-exchange.github.io/docs/futuresV2/inverse/#t-marginswitch
+*/
+func (c *InversePerpetualAccountClient) MarginSwitch(ctx context.Context, params *inverseperp.MarginSwitchParams) (response *inverseperp.MarginSwitchResponse, err error) {
+	err = c.Transporter.SignedRequest(ctx, http.MethodPost, bybit.PrivateInversePerpetualMarginSwitchPath, params, &response)
+	if err != nil {
+		return
+	}
+	return
+}
+
+/*
+QueryTradingFeeRate - query trading fee rate. [ /v2/private/execution/fee  - GET ]
+
+Query user's trading fee rate.
+
+docs - https://bybit-exchange.github.io/docs/futuresV2/inverse/#t-querytradingfeerate
+*/
+func (c *InversePerpetualAccountClient) QueryTradingFeeRate(ctx context.Context, params *inverseperp.QueryTradingFeeRateParams) (response *inverseperp.QueryTradingFeeRateResponse, err error) {
+	err = c.Transporter.SignedRequest(ctx, http.MethodGet, bybit.PrivateInversePerpetualQueryTradingFeeRatePath, params, &response)
+	if err != nil {
+		return
+	}
+	return
+}
